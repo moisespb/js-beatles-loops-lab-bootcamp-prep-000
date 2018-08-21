b9aaafe400f40dfc37708003fba25880e6f2abca
Moises Perez-Batista <moisesdpbjr@gmail.com>
2018-08-21 21:02:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ console.log($facts + "!!!")
 function iLoveTheBeatles(n){
   array1 = []
   n = i 
-  while(i <= 15 array1.push(i++));
-  console.log()
+  
+  console.log("I love the Beatles!") while(i <= 15 array1.push(i++));
   
 }
