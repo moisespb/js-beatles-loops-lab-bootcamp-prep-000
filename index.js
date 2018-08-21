2329e689a219c3b68fcde45517b5913d924de72f
Moises Perez-Batista <moisesdpbjr@gmail.com>
2018-08-21 19:40:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musician, instruments){
 var array = []
 array['musician'] = "John Lennon"
-array['$instruments'] = "guitar"
+array['instruments'] = "guitar"
 return console.log($musician + " plays " + $instruments)
 }
   
