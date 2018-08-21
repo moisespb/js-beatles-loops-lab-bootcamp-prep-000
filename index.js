90d5cf6f99ed2182bd55a8a762e6950bdaf9a750
Moises Perez-Batista <moisesdpbjr@gmail.com>
2018-08-21 20:36:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ return console.log($musician + " plays " + $instruments)
 }
   
 function johnLennonFacts(){
-const facts = [
-  "He was the last Beatle to learn to drive",
+const facts = 
+["He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
+  "He hated the sound of his own voice"];
+console.log($facts + "!!!")
 }
+
